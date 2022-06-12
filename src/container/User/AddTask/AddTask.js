c9dcf12fe4b956cc36afdf8cf2e50ccefe377830
Peragok56 <72642924+Peragok56@users.noreply.github.com>
2022-06-13 01:32:33 +0500

    safasfsaf

--- src/container/User/AddTask/AddTask.js
+++ src/container/User/AddTask/AddTask.js
@@ -11,7 +11,7 @@ class AddTask extends Component{
         super(props)
         this.state = {
             specifications: [],
-            select: []
+            select: [],
         }
     }
 
@@ -31,6 +31,30 @@ class AddTask extends Component{
             console.log(this.state.select);
         }
 
+        let send = (e) => {
+            e.preventDefault()
+            let title = document.getElementById('title').value
+            let description = document.getElementById('description').value
+            let address = document.getElementById('address').value
+            let datefrom = document.getElementById('dateFrom').value
+            let dateTo = document.getElementById('dateTo').value
+            let select = document.getElementById('select').value
+            let ageFrom = document.getElementById('ageFrom').value
+            let ageTo = document.getElementById('ageTo').value
+            let addSkil = document.getElementById('additionalSkills').value
+
+            axios.post('/helpRequest/create', 
+            {title: title, description: description, dateTime: {from: datefrom, to: dateTo}, address: address, specificationsIds: this.state.select, isOnline: select, ageCategory: {from: ageFrom, to: ageTo}, additionalSkills: addSkil},
+            {headers: {Authorization: localStorage.getItem('token')}})
+            .then((res) => {
+                console.log(res);
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+
+        }
+
 
         return(
             <div className={classes.Auth}>
@@ -41,13 +65,30 @@ class AddTask extends Component{
                         <form>
                             <input placeholder="Название" type='text' id="title"/>
                             <input placeholder="Описание" type='text' id="description"/>
-                            <input placeholder="Дата" type='date' id="dateTimr"/>
-                            <input placeholder="address" type='text' id="address"/>
-                            <h1>Направления: </h1>
+                            <input placeholder="Адрес" type='text' id="address"/>
+                            <h1 style={{color: 'rgb(0, 34, 121)'}}>Временные промежутки</h1>
+                            <div className={classes.ageCateg}>
+                                <input placeholder="От" type='date' id="dateFrom"/>
+                                <input placeholder="До" type='date' id='dateTo'/>
+                            </div>
+                            <h1 style={{color: '#002279'}}>Направления: </h1>
                                 {this.state.specifications.map((item) => 
                                     <h3 onClick={() => addSelect(item._id, item)} className='nameItem'>{item.name}</h3>
                                 )}   
-                            <button>Добавить</button>
+                            <h1 style={{color: 'rgb(0, 34, 121)'}}>Тип задачи</h1>
+                            <select id="select">
+                                <option value={true}>Онлайн</option>
+                                <option value={false}>Не онлайн</option>
+                            </select>
+                            <h1 style={{color: 'rgb(0, 34, 121)'}}>Возрастные категории</h1>
+                            <div className={classes.ageCateg}>
+                                <input placeholder="От" type='number'min="14" value='14' id="ageFrom"/>
+                                <input placeholder="До" type='number' max='65' value='65' id='ageTo'/>
+                            </div>
+                            <h1 style={{color: 'rgb(0, 34, 121)'}}>Дополнительные навыки</h1>
+                            <input placeholder="Дополнительные навыки" type='text' id="additionalSkills"/>
+
+                            <button onClick={send}>Добавить</button>
                         </form>
                     </div>
                     <div className={classes.Vector}></div>
